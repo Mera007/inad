f0ac5b036f7b2d152c6354035c84cc617aa40740
Mera007 <39528120+Mera007@users.noreply.github.com>
2018-10-11 11:39:42 +0800

    Update tencent_ads.cc

--- tencent_ads.cc
+++ tencent_ads.cc
@@ -4,8 +4,10 @@
 	"splash_id": "8863364436303842593",
 	"interstitial_id": "8575134060152130849",
 	"native_express_id": "7030020348049331",
+	"native_pos_id ": "5010320697302671 ",
 	"banner_enabled": true,
 	"splash_enabled": true,
 	"interstitial_enabled": true,
-	"native_express_enabled": true
+	"native_express_enabled": true,
+	"native_pos_enabled": true
 }
