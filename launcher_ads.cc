eaccad4fa53ae2f666aaf838aab20eec486628f4
Mera007 <39528120+Mera007@users.noreply.github.com>
2018-11-02 11:39:52 +0800

    Update launcher_ads.cc

--- launcher_ads.cc
+++ launcher_ads.cc
@@ -2,12 +2,29 @@
 	"lockscreen": [{
 			"ad": "Admob",
 			"quantity": 100,
+		        "max":100,
 			"appid": "ca-app-pub-3940256099942544~3347511713",
 			"id": "ca-app-pub-3940256099942544/2247696110"
 		},
 		{
 			"ad": "Facebook",
 			"quantity": 0,
+			"max":100,
+			"appid": "",
+			"id": ""
+		}
+	],
+	"search": [{
+			"ad": "Admob",
+			"quantity": 100,
+		        "max":100,
+			"appid": "ca-app-pub-3940256099942544~3347511713",
+			"id": "ca-app-pub-3940256099942544/2247696110"
+		},
+		{
+			"ad": "Facebook",
+			"quantity": 0,
+			"max":100,
 			"appid": "",
 			"id": ""
 		}
@@ -15,11 +32,13 @@
 	"native": [{
 			"ad": "Admob",
 			"quantity": 100,
+		        "max":100,
 			"appid": "ca-app-pub-3940256099942544~3347511713",
 			"id": "ca-app-pub-3940256099942544/2247696110"
 		},
 		{
 			"ad": "Facebook",
+		        "max":100,
 			"quantity": 0,
 			"appid": "",
 			"id": ""
@@ -28,12 +47,14 @@
 	"banner": [{
 			"ad": "Admob",
 			"quantity": 0,
+		        "max":100,
 			"appid": "",
 			"id": ""
 		},
 		{
 			"ad": "Facebook",
 			"quantity": 0,
+		        "max":100,
 			"appid": "",
 			"id": ""
 		}
@@ -42,12 +63,14 @@
 			"ad": "Admob",
 			"appid": "ca-app-pub-3940256099942544~3347511713",
 			"quantity": 100,
+		        "max":100,
 			"id": "ca-app-pub-3940256099942544/1033173712"
 		},
 		{
 			"ad": "Facebook",
 			"appid": "",
 			"quantity": 0,
+		        "max":100,
 			"id": ""
 		}
 	]
