1c5b6c26ea2a1b444a940dd3c8890895c18ed5f9
Mera007 <39528120+Mera007@users.noreply.github.com>
2018-11-30 00:29:18 +0800

    Update lockscreen_ads.cc

--- lockscreen_ads.cc
+++ lockscreen_ads.cc
@@ -13,6 +13,20 @@
       "id": ""
     }
   ],
+    "native_11": [
+    {
+      "ad": "Admob",
+      "quantity": 100,
+      "appid": "ca-app-pub-3940256099942544~3347511713",
+      "id": "ca-app-pub-3940256099942544/2247696110"
+    },
+    {
+      "ad": "Facebook",
+      "quantity": 0,
+      "appid": "",
+      "id": ""
+    }
+  ],
   "banner": [
     {
       "ad": "Admob",
