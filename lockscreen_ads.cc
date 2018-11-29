c072b7bc29592a5793996fe8066b88b5b44164a7
Mera007 <39528120+Mera007@users.noreply.github.com>
2018-11-29 23:03:30 +0800

    Create lockscreen_ads.cc

---
+++ lockscreen_ads.cc
@@ -0,0 +1,59 @@
+{
+  "native": [
+    {
+      "ad": "Admob",
+      "quantity": 100,
+      "appid": "ca-app-pub-3940256099942544~3347511713",
+      "id": "ca-app-pub-3940256099942544/2247696110"
+    },
+    {
+      "ad": "Facebook",
+      "quantity": 0,
+      "appid": "",
+      "id": ""
+    }
+  ],
+  "banner": [
+    {
+      "ad": "Admob",
+      "quantity": 100,
+      "appid": "ca-app-pub-3940256099942544~3347511713",
+      "id": "ca-app-pub-3940256099942544/6300978111"
+    },
+    {
+      "ad": "Facebook",
+      "quantity": 0,
+      "appid": "",
+      "id": ""
+    }
+  ],
+  "interstitial": [
+    {
+      "ad": "Admob",
+      "quantity": 100,
+      "appid": "ca-app-pub-3940256099942544~3347511713",
+      "id": "ca-app-pub-3940256099942544/1033173712"
+    },
+    {
+      "ad": "Facebook",
+      "quantity": 0,
+      "appid": "",
+      "id": ""
+    }
+  ],
+  "home_interstitial": [
+    {
+      "ad": "Admob",
+      "quantity": 100,
+      "appid": "ca-app-pub-3940256099942544~3347511713",
+      "id": "ca-app-pub-3940256099942544/1033173712"
+    },
+    {
+      "ad": "Facebook",
+      "quantity": 0,
+      "appid": "",
+      "id": ""
+    }
+  ]
+
+}
