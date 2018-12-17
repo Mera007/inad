d3a85ff7ae0563779326277be4a0d1141765c159
Mera007 <39528120+Mera007@users.noreply.github.com>
2018-12-17 14:15:04 +0800

    Create lockscreen_tencent_ads.cc

---
+++ lockscreen_tencent_ads.cc
@@ -0,0 +1,13 @@
+{
+  "app_id": "1108046010",
+  "banner_id": "1090345872779832",
+  "splash_id": "3040042842470886",
+  "interstitial_id": "7080742852774916",
+  "native_express_id": "9070745852078995",
+  "native_pos_id": "9080748812882539",
+  "banner_enabled": true,
+  "splash_enabled": true,
+  "interstitial_enabled": true,
+  "native_express_enabled": true,
+  "native_pos_enabled": true
+}
